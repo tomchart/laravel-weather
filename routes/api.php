509715c9bebe0b90abbd46664dd49ab531abce69
Tom Hart <tommy-hart@hotmail.com>
2022-06-21 14:35:13 +0100

    add route for api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Contracts\WeatherApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/weather/{location}', function (WeatherApi $weatherApi) {
+  return $weatherApi->searchLocation(request()->location);
 });
