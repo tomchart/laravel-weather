3b69d18b359c105d85b118f93834c417c6ad783e
Tom Hart <tommy-hart@hotmail.com>
2022-06-22 18:28:18 +0100

    amend function name & add forecastLocation to VisualCrossing service

--- app/Services/VisualCrossing.php
+++ app/Services/VisualCrossing.php
@@ -6,11 +6,21 @@ use Illuminate\Support\Facades\Http;
 
 class VisualCrossing implements \App\Contracts\WeatherApi
 {
-  public function searchLocation(string $location)
+  public function todayLocation(string $location)
+  {
+    $response = Http::get(config('services.vc.baseUri') . $location . '/today', [
+      'key' => config('services.vc.apiKey'),
+      'unitGroup' => 'uk',
+      'include' => 'fcst,obs,histfcst,stats,hours,current,alerts'
+      ]);
+    return $response;
+  }
+  public function forecastLocation(string $location)
   {
     $response = Http::get(config('services.vc.baseUri') . $location, [
       'key' => config('services.vc.apiKey'),
       'unitGroup' => 'uk',
+      'include' => 'fcst,obs,histfcst,stats,days,hours,current,alerts'
       ]);
     return $response;
   }
