a3f15a8bc181472017607672bdb938a18c5a14c4
Tom Hart <tommy-hart@hotmail.com>
2022-06-23 16:58:25 +0100

    today --> next24hours in VisualCrossing todayLocation function

--- app/Services/VisualCrossing.php
+++ app/Services/VisualCrossing.php
@@ -8,7 +8,7 @@ class VisualCrossing implements \App\Contracts\WeatherApi
 {
   public function todayLocation(string $location)
   {
-    $response = Http::get(config('services.vc.baseUri') . $location . '/today', [
+    $response = Http::get(config('services.vc.baseUri') . $location . '/next24hours', [
       'key' => config('services.vc.apiKey'),
       'unitGroup' => 'uk',
       'include' => 'fcst,obs,histfcst,stats,hours,current,alerts'
