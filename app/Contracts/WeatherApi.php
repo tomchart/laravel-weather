1040f64b210f246c1559dc499df840fe6fb60e3d
Tom Hart <tommy-hart@hotmail.com>
2022-09-02 16:58:15 +0100

    add parseResponse funciton to WeatherApi contract

--- app/Contracts/WeatherApi.php
+++ app/Contracts/WeatherApi.php
@@ -4,6 +4,7 @@ namespace App\Contracts;
 
 interface WeatherApi
 {
+  public function parseResponse(String $response);
   public function todayLocation(String $location);
   public function forecastLocation(String $location);
 }
